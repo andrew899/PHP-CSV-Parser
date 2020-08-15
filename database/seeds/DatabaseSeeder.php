2502401245d6ab398fcffc41d05589bd5896896d
Andrew Kovalenko <41870463+andrew899@users.noreply.github.com>
2020-08-15 13:51:10 +0300

    For work on Notebook

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('123456'),
+        ]);
     }
 }
